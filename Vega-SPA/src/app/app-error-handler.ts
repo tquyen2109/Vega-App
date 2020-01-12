7b54fca15e70dfdfe0de56f445e6511ac23dfd03
Quyen Tran <t.quyen1997@yahoo.com>
2020-01-12 08:32:04 -0500

    Fix Angular Zone

--- Vega-SPA/src/app/app-error-handler.ts
+++ Vega-SPA/src/app/app-error-handler.ts
@@ -1,16 +1,21 @@
 import { ToastyService } from 'ng2-toasty';
-import { ErrorHandler, Inject } from '@angular/core';
+import { ErrorHandler, Inject, NgZone } from '@angular/core';
 
 export class AppErrorHandler implements ErrorHandler{
  
-    constructor(@Inject(ToastyService) private toastyService: ToastyService) {}
+    constructor(
+        @Inject(NgZone) private ngZone: NgZone,
+        @Inject(ToastyService) private toastyService: ToastyService) {}
     handleError(_error: any): void {
-     this.toastyService.error({
-        title:'Error',
-        msg:'An unexpected error happened.',
-        theme: 'bootstrap',
-        showClose: true,
-        timeout: 5000
-      });
+        this.ngZone.run(() => {
+            this.toastyService.error({
+                title:'Error',
+                msg:'An unexpected error happened.',
+                theme: 'bootstrap',
+                showClose: true,
+                timeout: 5000
+              });
+        });
+     
     }
 }
