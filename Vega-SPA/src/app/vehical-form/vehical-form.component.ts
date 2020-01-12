7db5a24bf858e78384fb3c8c4775ddee41fb216e
Quyen Tran <t.quyen1997@yahoo.com>
2020-01-12 10:03:49 -0500

    Populate Edit form

--- Vega-SPA/src/app/vehical-form/vehical-form.component.ts
+++ Vega-SPA/src/app/vehical-form/vehical-form.component.ts
@@ -1,9 +1,11 @@
+import { SaveVehicle, Vehicle } from './../Models/vehicle';
 import { ToastyModule, ToastyService } from 'ng2-toasty';
 import { VehicleService } from '../services/vehicle.service';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, RouterModule, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import 'rxjs/add/Observable/forkJoin';
+import * as _ from 'underscore';
 @Component({
   selector: 'app-vehical-form',
   templateUrl: './vehical-form.component.html',
@@ -22,9 +24,17 @@ export class VehicalFormComponent implements OnInit {
               }
   makes: any[];
   features: any[];
-  vehicle: any ={
+  vehicle: SaveVehicle ={
+    id: 0,
+    makeId: 0,
+    modelId: 0,
+    isRegistered: false,
     features: [],
-    contact: {}
+    contact: {
+      name: '',
+      phone: '',
+      email: ''
+    }
   };
   models: any[];
   ngOnInit() {
@@ -34,23 +44,44 @@ export class VehicalFormComponent implements OnInit {
     ];
 
     if (this.vehicle.id)
+    {
       sources.push(this.vehicleService.getVehicle(this.vehicle.id));
+    }
     //forJoin to work with different Observable parallel
     Observable.forkJoin(sources).subscribe((data:any) => {
       this.makes = data[0];
       this.features = data[1];
-      if (this.vehicle.id)
-        this.vehicle = data[2];
+      if (this.vehicle.id){
+        this.setVehiclle(data[2]);
+        this.populateModels();
+      }
     }, err => {
-      if (err.status == 404)
-        this.router.navigate(['/home']);
+      if (err.status === 404)
+      {
+       this.router.navigate(['/home']);
+      }
     });
     }
-  onMakeChange(){
-    var selectedMake = this.makes.find(m => m.id == this.vehicle.makeId);
-    this.models = selectedMake ? selectedMake.models : [];
+
+  private setVehiclle(v:Vehicle){
+    this.vehicle.id = v.id;
+    this.vehicle.makeId = v.make.id;
+    this.vehicle.modelId = v.model.id;
+    this.vehicle.isRegistered = v.isRegistered;
+    this.vehicle.contact = v.contact;
+    this.vehicle.features = _.pluck(v.features,'id');
+
+  }
+  onMakeChange() {
+    this.populateModels();
     delete this.vehicle.modelId;
   }
+
+  private populateModels() {
+    var selectedMake = this.makes.find(m => m.id == this.vehicle.makeId);
+    this.models = selectedMake ? selectedMake.models : [];
+  }
+
   onFeatureToggle(featureId, $event)
   {
     if ($event.target.checked)
